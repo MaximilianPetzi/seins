1785c1b5c82cf1a15ce969137ccd3cd0d8c6886f
MaximilianPetzi <maximilianpetzi@gmx.de>
2021-10-26 03:10:53 -0700

    plotfix

--- plit.py
+++ plit.py
@@ -1,18 +1,47 @@
-#almost original version
 import numpy as np
 from matplotlib import pyplot as plt 
+import matplotlib
+#matplotlib.use("TkAgg")
+import os
+os.system("rm ../bilder/plit_temp/*")
 nrt=25
-am = np.zeros(nrt)
-nrs=2
-a = np.zeros((nrs,nrt))
-for i in range(nrs):
-    t = np.load(str(i+1)+'error.npy')
-    am += t
-    a[i] = t
-am = am/nrs
-separated = (am[11:2198:3] + am[12:2199:3] + am[13::3] )/3.
-mean_error = np.std(a,axis=0)/nrs**2
-mean_error_separated = (mean_error[11:2198:3] + mean_error[12:2199:3] + mean_error[13::3] )/3.
-markers, caps, bars = plt.errorbar(range(729),separated, yerr=mean_error_separated, label='Direct angle output')
-[bar.set_alpha(0.2) for bar in bars]
+am = []
+for i in range(nrt):
+    print(i+1)
+    try:
+        t = np.load("../error_h/"+str(i+1)+'error.npy')
+        am.append(t)
+    except:
+        print("--"+str(i+1)+"error.npy is missing--")
+am=np.array(am)
+
+minl=10000000
+for i in range(len(am)):
+    minl=np.min([minl,len(am[i])])
+Am=np.zeros((len(am),minl))
+for i in range(len(am)):
+    amm=am[i]
+    Am[i,:]=amm[:minl]
+print("data-shape before avg:",np.shape(Am))
+
+#sliding avg:
+siz=15
+slav=np.zeros(np.shape(Am[:,siz-1:]))
+for i in range(len(slav[0])):
+    slav[:,i]=slav[:,i]+np.sum(Am[:,i:i+siz],axis=1)
+slav=slav/siz
+
+yerr=np.std(slav,axis=0)
+print("AAAAAA",np.shape(slav))
+sliding_av=np.average(slav,axis=0)
+Am=np.average(Am,axis=0)
+
+
+
+plt.plot(sliding_av,color=(1,0,0,1),label="sliding avg")
+plt.plot(Am,color="black",linewidth=.4)
+plt.errorbar(x=range(len(sliding_av)),y=sliding_av, yerr=yerr,color=(1,0,0,.2),elinewidth=.5,label="error of sliding avg")
+plt.legend()
+plt.savefig("../bilder/plit_temp/this")
 plt.show()
+
